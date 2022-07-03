4833e17d3e7f73f873b244580f056441f854441d
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-03 01:18:15 -0500

    chore: create product page

---
+++ src/pages/Product.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { ProductProvider, useProductContext } from '../context/product';
+
+export default function Product() {
+  return (
+    <ProductProvider>
+      <ProductContent />
+    </ProductProvider>
+  );
+}
+
+function ProductContent() {
+  const { productId: id } = useParams();
+  const productId = Number(id);
+  const { loading, error, product, getProduct } = useProductContext();
+  useEffect(() => {
+    getProduct(productId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (product == null) {
+    return (
+      <div>
+        <h2>
+          {loading && <>Loading...</>}
+          {error && <>{error.message ?? 'Unexpected error'}</>}
+          {!loading && !error && <>Product not found</>}
+        </h2>
+      </div>
+    );
+  }
+
+  return (
+    <div key={product.id}>
+      <h2>{product.title}</h2>
+      <img src={product.image} alt={product.title} />
+    </div>
+  );
+}
