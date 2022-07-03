e73c1f6b115aa9b718e3580a0ad8f9492fa61a0e
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-02 23:42:03 -0500

    chore: display proper routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+
 function App() {
   return (
     <div className="App">
-      <h1>React Store</h1>
+      <Suspense fallback={<LoadingPagePlaceholder />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
+}
+
+function LoadingPagePlaceholder() {
+  return <h1>Loading...</h1>;
 }
 
 export default App;
