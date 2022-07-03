a633420a3ae2ce7e75ebc58b74078a10c7da73aa
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2022-07-02 23:57:38 -0500

    chore: setup HTTP client (#2)

---
+++ src/api/_client.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+export default apiClient;
