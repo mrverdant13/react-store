ede467c4a66c39322d21db988afac75dd2d3a78c
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-03 19:21:38 -0500

    chore: create profile page

---
+++ src/pages/Profile.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { ProfileProvider, useProfileContext } from '../context/profile';
+
+export default function Profile() {
+  return (
+    <ProfileProvider>
+      <h1>Who I am</h1>
+      <ProfileContent />
+    </ProfileProvider>
+  );
+}
+
+function ProfileContent() {
+  const { loading, error, profile, getProfile } = useProfileContext();
+  useEffect(() => {
+    getProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (profile == null) {
+    return (
+      <div>
+        <h2>
+          {loading && <>Loading...</>}
+          {error && <>{error.message ?? 'Unexpected error'}</>}
+          {!loading && !error && <>Unavailable profile</>}
+        </h2>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>{profile.name}</h2>
+      <p>{profile.bio}</p>
+      <a href={profile.url} target="_blank" rel="noopener noreferrer">
+        {profile.login}
+      </a>
+      <img src={profile.avatar_url} alt={profile.title} />
+    </div>
+  );
+}
